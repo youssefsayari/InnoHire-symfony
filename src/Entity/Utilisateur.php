adf93bb0c15388b08c5d2bd600a06fb49f7cb5a5
amenk21 <amenallah.kthiri@esprit.tn>
2024-03-22 23:12:47 +0000

    .

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "IDENTITY")]
+    #[ORM\Column(name: "id_utilisateur", type: "integer")]
+    private ?int $id_utilisateur = null;
+
+    #[ORM\Column(type: "integer", nullable: true)]
+    private ?int $cin = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $mdp = null;
+
+    #[ORM\Column(type: "integer", nullable: true)]
+    private ?int $role = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $image = null;
+
+    public function getIdUtilisateur(): ?int
+    {
+        return $this->id_utilisateur;
+    }
+
+    public function getCin(): ?int
+    {
+        return $this->cin;
+    }
+
+    public function setCin(int $cin): self
+    {
+        $this->cin = $cin;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getRole(): ?int
+    {
+        return $this->role;
+    }
+
+    public function setRole(int $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
