35fcd41479f68c68231531d92206ec52005d334a
youssef sayari <youssef.sayari@esprit.tn>
2024-03-23 02:06:19 +0100

    sayari_entities

---
+++ src/Entity/Etablissement.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtablissementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtablissementRepository::class)]
+class Etablissement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lieu = null;
+
+    #[ORM\Column]
+    private ?int $code_etablissement = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_etablissement = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Utilisateur $id_utilisateur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): static
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getCodeEtablissement(): ?int
+    {
+        return $this->code_etablissement;
+    }
+
+    public function setCodeEtablissement(int $code_etablissement): static
+    {
+        $this->code_etablissement = $code_etablissement;
+
+        return $this;
+    }
+
+    public function getTypeEtablissement(): ?string
+    {
+        return $this->type_etablissement;
+    }
+
+    public function setTypeEtablissement(string $type_etablissement): static
+    {
+        $this->type_etablissement = $type_etablissement;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getIdUtilisateur(): ?Utilisateur
+    {
+        return $this->id_utilisateur;
+    }
+
+    public function setIdUtilisateur(?Utilisateur $id_utilisateur): static
+    {
+        $this->id_utilisateur = $id_utilisateur;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Wallet.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WalletRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WalletRepository::class)]
+class Wallet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $balance = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Etablissement $id_etablissement = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_c = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBalance(): ?int
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(?int $balance): static
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getIdEtablissement(): ?Etablissement
+    {
+        return $this->id_etablissement;
+    }
+
+    public function setIdEtablissement(Etablissement $id_etablissement): static
+    {
+        $this->id_etablissement = $id_etablissement;
+
+        return $this;
+    }
+
+    public function getDateC(): ?\DateTimeInterface
+    {
+        return $this->date_c;
+    }
+
+    public function setDateC(\DateTimeInterface $date_c): static
+    {
+        $this->date_c = $date_c;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
