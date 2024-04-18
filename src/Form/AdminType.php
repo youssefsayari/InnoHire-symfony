ec250c1ec9f60ea6aba07ffe0bbbf1565b4b92fc
amenk21 <amenallah.kthiri@esprit.tn>
2024-04-18 14:04:24 +0100

    .

--- src/Form/AdminType.php
+++ src/Form/AdminType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -67,7 +69,22 @@ class AdminType extends AbstractType
                 'expanded' => true, // Optionally, if you want radio buttons instead of a dropdown
                 'multiple' => false, // Optionally, if you want to allow selecting multiple roles
             ])
-           // ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'User Image',
+                'mapped' => false, // This field is not mapped to any entity property
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file (jpeg, png, gif)',
+                    ])
+                ],
+            ]);
             
         ;
     }
