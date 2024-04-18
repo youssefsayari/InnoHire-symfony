da5bc7c7219ff2cce7c9bda289e4556bd949ccf3
amenk21 <amenallah.kthiri@esprit.tn>
2024-04-18 14:35:45 +0100

    khidmet_image_wilyey

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -189,6 +189,33 @@ public function edit(Request $request,int $id_utilisateur, Utilisateur $utilisat
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+        $imageFile = $form['image']->getData();
+            
+            if ($imageFile) {
+                // Use the original filename of the uploaded file
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // Use the file extension of the uploaded file
+                $fileExtension = $imageFile->guessExtension();
+    
+                // Concatenate the original filename and extension
+                $imageName = $originalFilename.'.'.$fileExtension;
+    
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'), // Defined in services.yaml
+                        $imageName
+                    );
+                } catch (FileException $e) {
+                    // Handle file upload error
+                    // You may want to customize this part based on your application's needs
+                    throw new Exception('Unable to upload the image file.');
+                }
+    
+                // Store the image file name in the database
+                $utilisateur->setImage($imageName);
+            }
+    
         $entityManager->flush();
 
         return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
