a5903ee6e77273980f0ff0514832aeb88ed2d7c8
HachemDhawadiSE <hachemdhawadi1@gmail.com>
2024-04-28 21:38:44 +0100

    Bundels

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -7,11 +7,24 @@ use App\Entity\Utilisateur;
 use App\Form\ReclamationType;
 use App\Repository\ReclamationRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCodeBundle\Response\QrCodeResponse;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Endroid\QrCode\Factory\QrCodeFactoryInterface;
+use Endroid\QrCode\Builder\BuilderInterface;
+use Endroid\QrCode\Color\Color;
+
 
 #[Route('/reclamation')]
 class ReclamationController extends AbstractController
@@ -90,14 +103,52 @@ class ReclamationController extends AbstractController
         ]);
     }
         ////////////////////////////////////////////////////////////////////////////////////
+        #[Route('/{id}', name: 'app_reclamation_show', methods: ['GET'])]
+        public function show(Reclamation $reclamation, BuilderInterface $qrCodeBuilder, ParameterBagInterface $parameterBag): Response
+        {
+            // Format the data string to include claim details
+            $claimData = sprintf(
+                "Claim ID: %d\nType: %s\nTitre: %s\nDescription: %s\nDate: %s\nStatus: %s",
+                $reclamation->getId(),
+                $reclamation->getType(),
+                $reclamation->getTitre(),
+                $reclamation->getDescription(),
+                $reclamation->getDate()->format('Y-m-d H:i:s'),
+                $reclamation->getStatus() == 0 ? 'untreated' : 'treated'
+            );
 
-    #[Route('/{id}', name: 'app_reclamation_show', methods: ['GET'])]
-    public function show(Reclamation $reclamation): Response
-    {
-        return $this->render('reclamation/show.html.twig', [
-            'reclamation' => $reclamation,
-        ]);
-    }
+           // dd( $claimData);
+        
+            // Generate QR code with claim data
+            $qrCode = $qrCodeBuilder->create($claimData)
+                ->backgroundColor(new Color(0, 0, 0, 127)) // Transparent background
+                ->size(200) // Adjust the size as needed
+                ->build();
+        
+            // Get the directory where the QR codes will be stored
+            $qrCodeDirectory = $parameterBag->get('kernel.project_dir') . '/public/qrcodes/';
+        
+            // Ensure the directory exists
+            if (!is_dir($qrCodeDirectory)) {
+                mkdir($qrCodeDirectory, 0777, true);
+            }
+        
+            // Generate a unique filename for the QR code image
+            $qrCodeFilename = 'qrcode_' . uniqid() . '.png';
+        
+            // Save the QR code image to the directory
+            $qrCode->saveToFile($qrCodeDirectory . $qrCodeFilename);
+        
+            // Get the relative path to the QR code image
+            $qrCodePath = '/qrcodes/' . $qrCodeFilename;
+        
+            return $this->render('reclamation/show.html.twig', [
+                'reclamation' => $reclamation,
+                'qrCodePath' => $qrCodePath,
+            ]);
+        }
+        
+        
 
     #[Route('/{id}/edit', name: 'app_reclamation_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
@@ -160,4 +211,57 @@ public function editfront(Request $request, Reclamation $reclamation, EntityMana
         return $this->redirectToRoute('app_reclamation_indexfront', [], Response::HTTP_SEE_OTHER);
     }
     
+    #[Route('/{id}/download-pdf', name: 'download_reclamation_pdf', methods: ['GET'])]
+public function downloadPdf(Reclamation $reclamation): Response
+{
+    // Render the Twig template to HTML
+    $html = $this->renderView('reclamation/pdf_template.html.twig', [
+        'reclamation' => $reclamation,
+    ]);
+
+    // Configure Dompdf options
+    $options = new Options();
+    $options->set('isHtml5ParserEnabled', true);
+    $options->set('isRemoteEnabled', true);
+
+    // Instantiate Dompdf
+    $dompdf = new Dompdf($options);
+
+    // Load HTML content
+    $dompdf->loadHtml($html);
+
+    // Set paper size and orientation
+    $dompdf->setPaper('A4', 'portrait');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Output the generated PDF (inline or as attachment)
+    return new Response($dompdf->output(), Response::HTTP_OK, [
+        'Content-Type' => 'application/pdf',
+    ]);
+}
+
+
+#[Route('/{id}/generate-qrcode', name: 'generate_qrcode', methods: ['GET'])]
+public function generateQRCode(Reclamation $reclamation): Response
+{
+    // Generate the QR code content (you can customize this according to your needs)
+    $qrCodeContent = $reclamation->getId(); // For example, you can use the ID of the reclamation
+    dd( $qrCodeContent);
+
+    // Create the QR code instance
+    $qrCode = Builder::create()
+        ->writer(new PngWriter())
+        ->data($qrCodeContent)
+        ->build();
+
+    // Get the data URI of the QR code image
+    $dataUri = $qrCode->getDataUri();
+
+    // Return the QR code image as a response
+    return new Response($dataUri, Response::HTTP_OK, [
+        'Content-Type' => 'image/png',
+    ]);
+}
 }
